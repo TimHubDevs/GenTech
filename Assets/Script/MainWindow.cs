b5015caa2cb215073c12f04447cf82ffdc1d4346
Timur Perepelitsa <devsspacegame@gmail.com>
2022-12-21 02:45:11 +0200

    Create animation infoBlock

--- Assets/Script/MainWindow.cs
+++ Assets/Script/MainWindow.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class MainWindow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private CanvasGroup InfoBlock;
+
+    private void Start()
     {
-        
+        DOTween.Init();
+        ChangeState(InfoBlock);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ChangeState(CanvasGroup canvasGroup)
     {
-        
+        bool isActive = InfoBlock.alpha != 0f;
+        if (isActive)
+        {
+            ChangeVisibility(canvasGroup, 0, false);
+        }
+        else
+        {
+            ChangeVisibility(canvasGroup, 1, true);
+        }
+    }
+
+    private void ChangeVisibility(CanvasGroup canvasGroup, float endAlpha, bool interactable)
+    {
+        Sequence mySequence = DOTween.Sequence();
+        mySequence.PrependCallback(() => { canvasGroup.interactable = false; })
+            .Append(DOTween.To(() => canvasGroup.alpha, x => canvasGroup.alpha = x, endAlpha, 3))
+            .AppendCallback(() => { canvasGroup.interactable = interactable; });
     }
 }
